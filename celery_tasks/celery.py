c27c43d920aa6c35f8021c2a79cb85186cf96ef5
xiangchen <805071841@qq.com>
2019-11-21 09:33:30 +0800

    update

--- celery_tasks/celery.py
+++ celery_tasks/celery.py
@@ -3,4 +3,4 @@ from celery import Celery
 app = Celery(__name__, include=['celery_tasks.tasks'])
 app.config_from_object('celery_tasks.config')
 
-# celery -A celery_tasks worker -P eventlet -l info -f logging/celery.log
+# celery -A celery_tasks worker -P eventlet -l info -f celery_tasks/celery.log
