b16e54aa1e5935cbbe82f5d959b9ae72affbd9e9
xiangchen <805071841@qq.com>
2019-11-27 15:07:29 +0800

    update

--- weibo_spider/project/settings.py
+++ weibo_spider/project/settings.py
@@ -55,7 +55,7 @@ DEFAULT_REQUEST_HEADERS = {
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-    'project.middlewares.ProxyMiddleware': 110,
+    'project.middlewares.ADSLProxyMiddleware': 110,
     'project.middlewares.UserErrorMiddleware': 120,
     'project.middlewares.AccountExceptionMiddleware': 130,
     'project.middlewares.CookieMiddleware': 650,
